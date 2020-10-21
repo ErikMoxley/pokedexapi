8d5b0d1e8a3ddfe7bfec0b5599645d6a225561f1
ErikMoxley <exilerzero@yahoo.com>
2020-10-21 10:10:26 -0400

    changed pokesprite url to less too many requests

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -34,7 +34,7 @@ export default class PokemonCard extends Component {
     componentDidMount () {
         const { name, url } = this.props;
         const pokemonIndex = url.split("/")[url.split('/').length - 2];
-        const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
 
         this.setState({
             name,  
